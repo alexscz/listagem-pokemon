426dce0aa5f4232e867d0f84997681e2e2663a74
Alex Ackerman <131216193+alexscz@users.noreply.github.com>
2023-05-14 14:58:13 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    //body.classList.toggle("modo-escuro")
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    if (modoEscuroEstaAtivo) {
+
+        body.classList.remove("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+
+        body.classList.add("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+
+    }
+
+
+});
+
